7647f3c75ecf015880eb3000663b0565df7e5138
Zane_Goodman <zgoodman1776@gmail.com>
2023-11-06 21:11:29 -0500

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Eightball from "./EightBall";
+
+function App() {
+  return (
+    <div className="App">
+      <Eightball />
+    </div>
+  );
+}
+
+export default App;
